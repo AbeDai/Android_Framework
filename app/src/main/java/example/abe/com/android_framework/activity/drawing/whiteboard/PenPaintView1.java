3638f5940b4f7f73060dabf6b9f4a8ffe2a05af1
abe <644670532@qq.com>
2016-08-19 17:33:12 +0800

    nothing

--- app/src/main/java/example/abe/com/android_framework/activity/drawing/whiteboard/PenPaintView1.java
+++ app/src/main/java/example/abe/com/android_framework/activity/drawing/whiteboard/PenPaintView1.java
@@ -10,21 +10,21 @@ import example.abe.com.android_framework.activity.drawing.whiteboard.tool.Type;
 import example.abe.com.android_framework.activity.drawing.whiteboard.tool.paint.PaintFeatures;
 import example.abe.com.android_framework.activity.drawing.whiteboard.tool.view.AbPaintView;
 
-public class PenPaintView extends AbPaintView {
+public class PenPaintView1 extends AbPaintView {
 
     private DrawManager mDrawManager;
 
-    public PenPaintView(Context context) {
+    public PenPaintView1(Context context) {
         super(context);
         init(null, 0);
     }
 
-    public PenPaintView(Context context, AttributeSet attrs) {
+    public PenPaintView1(Context context, AttributeSet attrs) {
         super(context, attrs);
         init(attrs, 0);
     }
 
-    public PenPaintView(Context context, AttributeSet attrs, int defStyle) {
+    public PenPaintView1(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         init(attrs, defStyle);
     }
