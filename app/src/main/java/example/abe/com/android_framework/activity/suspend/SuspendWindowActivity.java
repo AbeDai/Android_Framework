361700c692666194deaad133a5158931b2d5af46
abe <644670532@qq.com>
2016-09-29 15:59:04 +0800

    更新悬浮窗

--- app/src/main/java/example/abe/com/android_framework/activity/suspend/SuspendWindowActivity.java
+++ app/src/main/java/example/abe/com/android_framework/activity/suspend/SuspendWindowActivity.java
@@ -1,7 +1,6 @@
 package example.abe.com.android_framework.activity.suspend;
 
 import android.content.Intent;
-import android.view.View;
 
 import com.example.OnClick;
 
@@ -9,7 +8,6 @@ import example.abe.com.android_framework.R;
 import example.abe.com.framework.main.BaseActivity;
 
 public class SuspendWindowActivity extends BaseActivity {
-
 
     @Override
     public int getLayoutID() {
@@ -24,20 +22,16 @@ public class SuspendWindowActivity extends BaseActivity {
     public void initView() {
     }
 
-    @OnClick(R.id.act_suspend_window_show_memory_window)
+    @OnClick(R.id.act_suspend_window_start_memory_window_service)
     public void OnSimpleSuspendWindowClick() {
         Intent intent = new Intent(this, MemoryService.class);
         startService(intent);
     }
 
-    @OnClick(R.id.act_suspend_window_change_simple_suspend_window)
+    @OnClick(R.id.act_suspend_window_stop_memory_window_service)
     public void OnChangeSimpleSuspendWindowClick() {
-
-    }
-
-    @OnClick(R.id.act_suspend_window_remove_simple_suspend_window)
-    public void OnRemoveSimpleSuspendWindowClick() {
-
+        Intent intent = new Intent(this, MemoryService.class);
+        stopService(intent);
     }
 }
 
